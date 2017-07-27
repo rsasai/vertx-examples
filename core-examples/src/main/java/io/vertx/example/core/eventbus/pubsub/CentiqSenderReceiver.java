aeb92e79c36b09281225ad3babb3376992155a7e
Ryu Sasai <rsasai@centiq.co.uk>
2017-07-27 18:18:20 +0100

    refactoring

--- core-examples/src/main/java/io/vertx/example/core/eventbus/pubsub/CentiqSenderReceiver.java
+++ core-examples/src/main/java/io/vertx/example/core/eventbus/pubsub/CentiqSenderReceiver.java
@@ -2,6 +2,7 @@ package io.vertx.example.core.eventbus.pubsub;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
 import io.vertx.example.util.Runner;
 
 /**
@@ -19,22 +20,23 @@ public class CentiqSenderReceiver extends AbstractVerticle {
 	public void start() throws Exception {
 
 		EventBus eb = vertx.eventBus();
+		String eb_addr = "addr-for-commands";
 
 		// sender role
 		vertx.setPeriodic(
-				(int)(Math.random() * ((20000 - 5000) + 1)) + 5000,
-				v -> eb.publish("news-feed",
+				getRandom(20000, 5000),
+				v -> eb.publish(eb_addr,
 						String.format("%s: %s %s %s",
 								getObjNameForHuman(this),
 								"\"Change schedule of memory agent to every",
-								(int)(Math.random() * ((600 - 10) + 1)) + 10,
+								getRandom(600, 10),
 								"seconds\"")));
 
 		// receiver role
-		eb.consumer("news-feed", message -> {
-			if ( message.body().toString().startsWith( getObjNameForHuman(this) ) ) {
+		eb.consumer(eb_addr, message -> {
+			if ( isMyCommand(message) ) {
 				// my publication; ignore
-				System.out.println("...");	
+				System.out.println("... ignoring my own command ...");
 				return;
 			}
 			System.out.println("Received command from " + message.body());	
@@ -44,7 +46,15 @@ public class CentiqSenderReceiver extends AbstractVerticle {
 		System.out.printf( "I am host-\"%s\"\n", getObjNameForHuman(this) );	
 	}
 
+	private boolean isMyCommand(Message<Object> message) {
+		return message.body().toString().startsWith( getObjNameForHuman(this) );
+	}
+
 	private String getObjNameForHuman(Object obj) {
 		return "host-" + obj.toString().replaceAll("^.*@", "").substring(0, 3);
+	}
+
+	private int getRandom(int max, int min) {
+		return (int)(Math.random() * ((max - min) + 1)) + min;
 	}
 }
